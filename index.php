04c735667ac76d6c117b17f9c3e1b04a5326cad0
JoranBoonstoppel <boonstoppeljoran@gmail.com>
2023-02-07 11:30:00 +0100

    Nav bar maded

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 
+session_start();
+include ("header.php");
 
-$servername = "localhost";
-$dbname = "Bas";
-$username = "root";
-$password = "";
+?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Startpagina</title>
+</head>
+<body>
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    echo "connectie gelukt";
-} catch (PDOException $e) {
-    echo "Connectie mislukt: " . $e->getMessage();
-}
-try {
-    $con = mysqli_connect($servername, $username, $password, $dbname);
-//    echo "connectie gelukt";
-
-} catch (PDOException $e) {
-    echo "Connectie mislukt: " . $e->getMessage();
-}
+</body>
+</html>
